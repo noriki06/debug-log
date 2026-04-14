8fe91033b61c2906e674f4c74d675b917821bd19
大平尭宜 <oohiranoriki@oohiratakashiyonoMacBook-Air.local>
2026-04-14 22:10:58 +0900

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,24 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         @posts = @user.posts.order(created_at: :desc)
     end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to user_path(@user), notice: "ユーザーを作成しました"
+        else
+            render :new, status: :unprocessable_entity, alert: "ユーザーの作成に失敗しました"
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
+  get "/login", to: "sessions#new", as: :login
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy", as: :logout
+
+  get "/signup", to: "users#new", as: :signup
+  post "/signup", to: "users#create"
+
+
   resources :articles
   devise_for :users
   resources :categories
   get "home/index"
   root "home#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
   get "/posts", to: "posts#index", as: :posts
   get "/posts/new", to: "posts#new", as: :new_post
   post "/posts", to: "posts#create"
